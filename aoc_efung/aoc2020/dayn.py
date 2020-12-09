36e76bcdd0cd0edaa5a5409cf350d61b6d8e31b4
Eric Fung <efung@acm.org>
2020-12-08 22:50:15 -0500

    Template

---
+++ aoc_efung/aoc2020/dayn.py
@@ -0,0 +1,24 @@
+from aocd import data
+import re
+
+
+def process(data):
+    return None
+
+def part_a(data):
+    return None
+
+def part_b(data):
+    return None
+
+test_data = """\
+"""
+
+test_data_b = """\
+"""
+
+if __name__ == "__main__":
+    assert part_a(test_data) == "2"
+    assert part_b(test_data_b) == "4"
+    print(part_a(data))
+    print(part_b(data))
